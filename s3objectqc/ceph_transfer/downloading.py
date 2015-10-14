96fe11cac043883c959581e23ac4782ad59df3b3
lindaxiang <linda.xiang@oicr.on.ca>
2015-10-13 23:58:42 -0400

    create ceph_transfer class

---
+++ s3objectqc/ceph_transfer/downloading.py
@@ -0,0 +1,147 @@
+import sys
+import os
+import json
+import xmltodict
+import time
+import subprocess
+import calendar
+import hashlib
+from ..job_tracker import move_to_next_step, get_job_json
+
+
+name = 'downloading'
+next_step = 'uploading'
+
+
+def get_name():
+    global name
+    return name
+
+
+def download_metadata_xml(gnos_repo, gnos_id, job_dir, file_name):
+    file_info = {
+        'file_size': None,
+        'file_md5sum': None
+    }    
+    fpath = os.path.join(job_dir, file_name)
+    if not os.path.isfile(fpath):
+        url = gnos_repo + '/cghub/metadata/analysisFull/' + gnos_id
+        response = None
+        try:
+            response = requests.get(url, stream=False, timeout=30)
+        except: # download failed, no need to do anything
+            pass
+
+        if not response or not response.ok:
+            logger.warning('unable to download metadata for: {} from {}'.format(gnos_id, url))
+            return 
+        else:
+            metadata_xml_str = response.text
+            data = re.sub(r'<ResultSet .+?>', '<ResultSet>', metadata_xml_str)
+            with open(fpath, 'w') as f: f.write(data)
+
+    # TODO: get file size and md5sum
+    file_info['file_size'] = os.path.getsize(fpath)
+    file_info['file_md5sum'] = get_md5(fpath)
+
+    return file_info     
+
+
+def download_datafiles(gnos_repo, gnos_id, job_dir, file_name):
+
+    # global s3_bucket_url
+    # for f in job.job_json.get('files'):
+    #     if '.xml' in f.get('file_name'): continue
+    file_info = {}
+    fpath = os.path.join(job_dir, file_name)
+    start_time = int(calendar.timegm(time.gmtime()))
+    # Only download when file does not already exist.
+    # - This is meant more for repeative testing/debugging without
+    #   having to download large file over and over again.
+    # - In real world, shouldn't have as each time a new run dir is created 
+    if not os.path.isfile(fpath):
+        pass
+        # command =   'cd {} && '.format(job_dir) + \
+        #             'aws s3 cp ' + s3_bucket_url + \
+        #             object_id + ' ' + \
+        #             file_name
+
+        # process = subprocess.Popen(
+        #         command,
+        #         shell=True,
+        #         stdout=subprocess.PIPE,
+        #         stderr=subprocess.PIPE
+        #     )
+
+        # out, err = process.communicate()
+
+        # if process.returncode:
+        #     # should not exit for just this error, improve it later
+        #     sys.exit('Unable to download file from s3.\nError message: {}'.format(err))
+    end_time = int(calendar.timegm(time.gmtime()))
+    file_info['download_time'] = end_time - start_time
+    return file_info
+
+
+def compare_file(job):
+    # we'd like to do
+    # - download metadata file, check size and md5sum
+    # - download data files if metadata file match
+    job_dir = job.job_dir
+    gnos_id = job.job_json.get('gnos_id')
+    gnos_repo = job.job_json.get('gnos_repo')
+
+    for f in job.job_json.get('files'):
+        if not f.get('file_name').endswith('.xml'): continue        
+        file_name = f.get('file_name')
+        file_info = download_metadata_xml(gnos_repo, gnos_id, job_dir, file_name)
+
+        mismatch = False
+        if not file_info.get('file_size') == f.get('file_size'):
+            job.job_json.get('_runs_').get(job.conf.get('run_id')).get(get_name()).update({
+                f.get(file_name) + '-size-mismatch': file_info.get('file_size')
+            })
+            mismatch = True
+
+        if not file_info.get('file_md5sum') == f.get('file_md5sum'):
+            job.job_json.get('_runs_').get(job.conf.get('run_id')).get(get_name()).update({
+                f.get(file_name) + '-md5sum-mismatch': file_info.get('file_md5sum')
+            })
+            mismatch = True
+
+        if mismatch: return False
+
+    for f in job.job_json.get('files'):
+        if f.get('file_name').endswith('.xml'): continue
+        file_name = f.get('file_name')
+        ftype = 'bam' if file_name.endswith('.bam') else 'bai'
+        file_info = download_datafiles(gnos_repo, gnos_id, job_dir, file_name)
+        if file_info.get('download_time') is not None:
+            job.job_json.get('_runs_').get(job.conf.get('run_id')).get(get_name()).update({
+                ftype + '-download_time': file_info.get('download_time')
+            })
+        else:
+            return False
+
+    return True
+
+
+def _start_task(job):
+    job.job_json.get('_runs_').get(job.conf.get('run_id'))[get_name()] = {
+        'start': int(calendar.timegm(time.gmtime()))
+    }
+
+
+def run(job):
+    global name, next_step
+    print ('running task: {}'.format(get_name()))
+
+    _start_task(job)
+
+    if not compare_file(job): # file does not match
+        move_to_next_step(job, 'failed')
+        return False
+
+    # if everything was fine, finally move the job json file to the next_step folder
+    move_to_next_step(job, next_step)
+    return True
---
+++ s3objectqc/ceph_transfer/uploading.py
@@ -0,0 +1,87 @@
+import sys
+import os
+import json
+import xmltodict
+import time
+import subprocess
+import calendar
+import hashlib
+from ..job_tracker import move_to_next_step, get_job_json, save_job_json
+
+
+name = 'uploading'
+next_step = None
+ceph_bucket_url = 's3://oicr.icgc/data/'
+
+def get_name():
+    global name
+    return name
+
+def upload_job(job):
+    job_dir = job.job_dir
+    for f in job.job_json.get('files'):
+        object_id = f.get('object_id')
+        file_name = f.get('file_name')
+        ftype = file_name.split('.')[-1]
+        file_info = upload_file(job_dir, file_name, object_id)
+        if file_info.get('upload_time') is not None:
+            job.job_json.get('_runs_').get(job.conf.get('run_id')).get(get_name()).update({
+                ftype + '-upload_time': file_info.get('upload_time')
+            })
+        else:
+            return False
+
+    return True
+
+
+def upload_file(job_dir, file_name, object_id):
+    file_info = {}
+    fpath = os.path.join(job_dir, file_name)
+    start_time = int(calendar.timegm(time.gmtime()))
+    if not os.path.isfile(fpath):
+        return
+    else:
+        command =   'cd {} && '.format(job_dir) + \
+                    'aws --endpoint-url https://www.cancercollaboratory.org:9080 s3 cp ' + \
+                    file_name + ' ' + \
+                    ceph_bucket_url + object_id 
+
+        process = subprocess.Popen(
+                command,
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+
+        out, err = process.communicate()
+
+        if process.returncode:
+            return
+            # should not exit for just this error, improve it later
+            # sys.exit('Unable to upload file to ceph.\nError message: {}'.format(err))
+    end_time = int(calendar.timegm(time.gmtime()))
+    file_info['upload_time'] = end_time - start_time
+    return file_info
+
+
+def _start_task(job):
+    job.job_json.get('_runs_').get(job.conf.get('run_id'))[get_name()] = {
+        'start': int(calendar.timegm(time.gmtime()))
+    }
+
+
+def run(job):
+    global name, next_step
+    print ('running task: {}'.format(get_name()))
+
+    _start_task(job)
+
+    if not upload_job(job): # file does not be successfully uploaded 
+        move_to_next_step(job, 'failed')
+    else:
+        for f in job.job_json.get('files'):
+            if not f.get('file_name').endswith('.bam'): continue   
+            local_bam_file = os.path.join(job.job_dir, f.get('file_name'))
+            # remove the HUGH bam file when match
+            os.remove(local_bam_file)
+            move_to_next_step(job, 'completed')
